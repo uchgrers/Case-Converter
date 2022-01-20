513f90268922453388f0ceec44704bc4c9d3fcd3
uchgrers <97949004+uchgrers@users.noreply.github.com>
2022-01-20 18:17:22 +0300

    Case Converter

---
+++ main.js
@@ -0,0 +1,53 @@
+let upper = document.getElementById("upper-case");
+let lower = document.getElementById("lower-case");
+let proper = document.getElementById("proper-case");
+let sentence = document.getElementById("sentence-case");
+let save = document.getElementById("save-text-file");
+
+upper.addEventListener("click", function () {
+    document.getElementById("secondP").innerHTML = "text"
+})
+
+lower.addEventListener("click", function () {
+    let words = document.querySelector("textarea").value.toLowerCase();
+    document.querySelector("textarea").value = words;
+})
+
+upper.addEventListener("click", function () {
+    let words = document.querySelector("textarea").value.toUpperCase();
+    document.querySelector("textarea").value = words;
+})
+
+proper.addEventListener("click", function () {
+    let words = document.querySelector("textarea").value.toLowerCase().split(" ");
+    words.forEach(function (item, i) {
+        words[i] = item.charAt(0).toUpperCase() + item.substring(1);
+    })
+    document.querySelector("textarea").value = words.join(" ");
+})
+
+sentence.addEventListener("click", function () {
+    let words = document.querySelector("textarea").value.toLowerCase().split(". ");
+    words.forEach(function (item, i) {
+        words[i] = item.charAt(0).toUpperCase() + item.substring(1);
+    })
+    document.querySelector("textarea").value = words.join(". ")
+})
+
+function download(filename, text) {
+    let element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+
+save.addEventListener("click", function () {
+    let text = document.querySelector("textarea").value;
+    download('text.txt', text);
+}, false);
